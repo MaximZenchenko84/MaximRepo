f2754c45522e34b968c9c998672e5d89e4128fad
Maxim Zenchenko <nevelsk45@gmail.com>
2023-06-14 13:54:11 +0300

    feat: TaskCS1 created

--- TaskCS1_n2.cs
+++ TaskCS1_n2.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдает,
+// какое число большее, а какое меньшее
+
+using System;
+
+namespace Task1
+{
+    class TaskCS1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите число b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("max = ");
+            if (a > b)
+            {
+                
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
+
+
